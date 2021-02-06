38d38c0bd205e5181de8375e365718f72c09969a
satou54 <satou54satou@gmail.com>
2021-02-06 18:22:12 +0900

    各モデルにバリデーションを追加 #21

--- app/models/action_record.rb
+++ app/models/action_record.rb
@@ -1,4 +1,7 @@
 class ActionRecord < ApplicationRecord
   belongs_to :user
   belongs_to :task, primary_key: :task_id
+
+  validates :action_day, presence: true
+  validates :action, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   self.primary_key = :task_id
   belongs_to :user
+
+  validates :task, presence: true
+  validates :goal, presence: true
 end
