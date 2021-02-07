d84eb9777a4a274327987778c9630aa451beec53
satou54 <satou54satou@gmail.com>
2021-02-07 12:16:40 +0900

    目標新規作成ページ実装 #30

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -2,4 +2,20 @@ class Api::TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      render :index, status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.fetch(:task, {}).permit(:task, :goal, :user_id)
+  end
 end
