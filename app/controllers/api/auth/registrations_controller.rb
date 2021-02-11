931b4f9e7442f33f720ba3d94e7ad58409b2e330
satou54 <satou54satou@gmail.com>
2021-02-11 12:34:43 +0900

    registrations_controller.rbの修正 #46

--- app/controllers/api/auth/registrations_controller.rb
+++ app/controllers/api/auth/registrations_controller.rb
@@ -1,11 +1,15 @@
-class Api::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
-  private
+module Api
+  module Auth
+    class RegistrationsController < DeviseTokenAuth::RegistrationsController
+      private
 
-  def sign_up_params
-    params.permit(:name, :email, :password, :password_confirmation)
-  end
+      def sign_up_params
+        params.permit(:name, :email, :password, :password_confirmation)
+      end
 
-  def account_update_params
-    params.permit(:name, :email)
+      def account_update_params
+        params.permit(:name, :email)
+      end
+    end
   end
 end
