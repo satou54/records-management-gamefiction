1920d7f821a4dfe53ab8308ed7d8736a4acdfb50
satou54 <satou54satou@gmail.com>
2021-02-20 11:14:31 +0900

    ユーザ新規登録時にuser_levelsテーブルに新規登録する処理を実装 #74

--- app/javascript/packs/components/sinup.vue
+++ app/javascript/packs/components/sinup.vue
@@ -1,33 +1,35 @@
 <template>
 <div>
     <h1>新規登録画面</h1>
-    <div>
-      <label for="name">
-        Name
-      </label>
-      <input v-model="user.name" id="name" type="text" placeholder="name">
-    </div>
-    <div>
-      <label for="email">
-        Email
-      </label>
-      <input v-model="user.email" id="Email" type="text" placeholder="email">
-    </div>
-    <div>
-      <label for="password">
-        Password
-      </label>
-      <input v-model="user.password" id="password" type="password" placeholder="password">
-    </div>
-    <div>
-      <label for="password">
-        Password(確認用)
-      </label>
-      <input v-model="user.password_confirmation" id="password_confirmation" type="password" placeholder="password_confirmation">
-    </div>
-    <button @click="registerUser">
-      Sign Up
-    </button>
+    <form>
+      <div>
+        <label for="name">
+          ユーザ名
+        </label>
+        <input v-model="user.name" id="name" type="text" placeholder="name">
+      </div>
+      <div>
+        <label for="email">
+          メールアドレス
+        </label>
+        <input v-model="user.email" id="Email" type="text" placeholder="email">
+      </div>
+      <div>
+        <label for="password">
+          パスワード
+        </label>
+        <input v-model="user.password" id="password" type="password" placeholder="password">
+      </div>
+      <div>
+        <label for="password">
+          パスワード(確認用)
+        </label>
+        <input v-model="user.password_confirmation" id="password_confirmation" type="password" placeholder="password_confirmation">
+      </div>
+      <button @click="registerUser">
+        Sign Up
+      </button>
+    </form>
   </div>
 </template>
 
@@ -56,8 +58,18 @@
           localStorage.setItem('user_id', response.data['data'].id)
           localStorage.setItem('email', response.data['data'].email)
           localStorage.setItem('name', response.data['data'].name)
-          location.href = "http://localhost:3000/mypage"
-          return response
+          console.log('ログイン完了')
+          // user_levelsテーブルに新規作成したユーザの情報を作成
+          axios.post('api/user_levels', { level: 1, total_experience_point: 0, 
+                                          user_id: localStorage.getItem('user_id') 
+                                        }).then((response) => {
+            console.log('user_levelsテーブルに新規作成完了')
+          }, (error) => {
+            console.log('失敗')
+            console.log(error)
+          })
+          //location.href = "http://localhost:3000/mypage"
+          // return response
         }, (error) => {
           console.log(error)
         })
