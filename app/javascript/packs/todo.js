a639bf09b2f88162c388c4c325d1dca84c867563
satou54 <satou54satou@gmail.com>
2021-02-25 04:08:11 +0900

    未ログイン時にログイン画面に遷移するように実装 #124

--- app/javascript/packs/todo.js
+++ app/javascript/packs/todo.js
@@ -2,6 +2,15 @@ import Vue from 'vue/dist/vue.esm.js'
 import router from './router/router'
 import Header from './components/header.vue'
 
+router.beforeEach((to, from, next) => {
+    // isPublicでない場合(=認証が必要な場合)、かつ、ローカルストレージにアクセストークンを保持していない場合
+    if (to.matched.some(record => !record.meta.isPublic) && !localStorage.getItem('access-token')) {
+      next({ path: '/login', query: { redirect: to.fullPath }});
+    } else {
+      next();
+    }
+});
+
 var app = new Vue ({
   router,
   el: '#app',
