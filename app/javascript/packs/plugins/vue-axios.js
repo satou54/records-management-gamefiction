3867b44eebc1820be769c02b73bad6985d9c9e39
satou54 <satou54satou@gmail.com>
2021-03-06 02:19:51 +0900

    ソースファイルの変更が反映されない不具合を修正 #181

--- app/javascript/packs/plugins/vue-axios.js
+++ app/javascript/packs/plugins/vue-axios.js
@@ -1,9 +1,20 @@
 const VueAxiosPlugin = {}
 export default VueAxiosPlugin.install = function(Vue, { axios }) {
   const csrf_token = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-  axios.defaults.headers.common = {
-    "X-Requested-With": "XMLHttpRequest",
-    "X-CSRF-Token": csrf_token
+  
+  if (localStorage.getItem('access-token')) {
+    axios.defaults.headers.common = {
+      "X-Requested-With": "XMLHttpRequest",
+      "X-CSRF-Token": csrf_token,
+      "access-token": localStorage.getItem('access-token'),
+      "uid": localStorage.getItem('uid'),
+      "client": localStorage.getItem('client')
+    }
+  } else {
+    axios.defaults.headers.common = {
+      "X-Requested-With": "XMLHttpRequest",
+      "X-CSRF-Token": csrf_token
+    }
   }
 
   Vue.axios = axios
